f1ca54a11ef037d48cec17f2db03e0ceca7c9498
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-23 15:00:55 +0100

    add carousel functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const slidesElement = document.querySelector('.slides')
+const nextBtn = document.querySelector('.next')
+const prevBtn = document.querySelector('.prev')
 
-// get the images as an array
 const slides = [
     '01.webp',
     '02.webp',
@@ -15,4 +16,23 @@ for (let i = 0; i < slides.length; i++) {
     const slide = slides[i];
     const imageMarkup = `<img class="${i === activeImage && 'active'}" src="./assets/img/${slide}" alt="superhero image">`
     slidesElement.insertAdjacentHTML('beforeend', imageMarkup)
+}
+
+nextBtn.addEventListener('click', () => {
+    activeImage++
+    activeImage > (slides.length - 1) && (activeImage = 0)
+    toggleActiveClass()
+})
+
+prevBtn.addEventListener('click', () => {
+    activeImage--
+    activeImage < 0 && (activeImage = slides.length - 1)
+    toggleActiveClass()
+})
+
+function toggleActiveClass() {
+    const currentImage = document.querySelector('.active')
+    currentImage.classList.remove('active')
+    const allImages = document.querySelectorAll('.slides img')
+    allImages[activeImage].classList.add('active')
 }
